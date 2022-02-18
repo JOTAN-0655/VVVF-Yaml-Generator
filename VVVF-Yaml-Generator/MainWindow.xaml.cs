b770983ebdbfb3f054fda465034edc96a5662397
JOTAN <77259842+JOTAN-0655@users.noreply.github.com>
2022-02-18 21:12:57 +0900

    プロジェクト ファイルを追加します。

---
+++ VVVF-Yaml-Generator/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace VVVF_Data_Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void setting_button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string name = button.Name;
+            if (name.Equals("settings_level"))
+                setting_window.Navigate(new Uri("Pages/Settings/level_setting.xaml", UriKind.Relative));
+            else if(name.Equals("settings_minimum"))
+                setting_window.Navigate(new Uri("Pages/Settings/minimum_freq_setting.xaml", UriKind.Relative));
+            else if(name.Equals("settings_mascon"))
+                setting_window.Navigate(new Uri("Pages/Settings/mascon_off_setting.xaml", UriKind.Relative));
+            Debug.Print(((Button)sender).Name + "がクリックされました。");
+        }
+    }
+}
---
+++ VVVF-Yaml-Generator/Pages/Settings/mascon_off_setting.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VVVF_Data_Generator.Pages.Settings
+{
+    /// <summary>
+    /// mascon_off_setting.xaml の相互作用ロジック
+    /// </summary>
+    public partial class mascon_off_setting : Page
+    {
+        public mascon_off_setting()
+        {
+            InitializeComponent();
+        }
+    }
+}
